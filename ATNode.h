55243b8fd971b5964e77f1dff92f14d36eaec59b
slcc2c <slcc2c@mail.umkc.edu>
2017-03-03 12:48:58 -0600

    Tested constructor, properly initiates data structure

--- ATNode.h
+++ ATNode.h
@@ -4,16 +4,33 @@
 
 /** A node for an anagram Tree. */
 struct ATNode {    // Data Fields
-    char letter;
+    char letter = '-1';
     std::string remaining_letters;
     int num_children;
     //stores child nodes
     ATNode *children;
 	// Constructor
     ATNode() {}
-    ATNode(const std::string the_data, int letter_pos) :
-            remaining_letters(the_data), num_children(the_data.size()), children(new ATNode[the_data.size()]),
-            letter(the_data[letter_pos]) {}
+
+    ATNode(const std::string word, int letter_pos) : num_children(word.size() - 1), letter(word[letter_pos]) {
+
+        if (word.size() > 0) {
+            if (word.size() == 1) {
+                remaining_letters = "";
+            } else {
+                remaining_letters = removeLetter(letter_pos, word);
+            }
+        }
+
+        children = new ATNode[word.size()];
+        if (remaining_letters.size() > 0) {
+            for (int i = 0; i < num_children; i++) {
+                children[i] = ATNode(remaining_letters, i);
+            }
+        } else {
+            children = nullptr;
+        }
+    }
 	// Destructor (to avoid warning message)
     virtual ~ATNode() {}
 	// to_string
@@ -22,6 +39,22 @@ struct ATNode {    // Data Fields
         os << letter;
 		return os.str();
 	}
+
+
+    std::string removeLetter(int pos, std::string word) {
+        if (pos == 0) {
+            return word.substr(1, word.npos);
+        } else if (pos == word.npos) {
+            return word.substr(0, word.npos - 1);
+        } else {
+            return word.substr(0, pos) + word.substr(pos + 1, word.npos - pos - 1);
+        }
+    }
+
+    //TODO make it find grams
+    std::string *findAnagrams() {
+
+    }
 }; // End BTNode
 // Overloading the ostream insertion operator
 template<typename Item_Type>
@@ -29,5 +62,6 @@ std::ostream& operator<<(std::ostream& out,
                          const ATNode &node) {
 		return out << node.to_string();
 }
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 int main() {
     string word = "abc";
+
     ATNode root = ATNode(word, 0);
 
-
+    std::cout << root.children[1].letter << endl;
     return 0;
 }
